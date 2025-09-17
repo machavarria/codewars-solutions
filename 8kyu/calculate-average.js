b7e143a1db009c2fefa3b8a7acba290669b6d360
Marc Chavarria <marc.a.chavarria3@gmail.com>
2025-09-17 19:45:11 -0400

    Added new codewars challenge

---
+++ 8kyu/calculate-average.js
@@ -0,0 +1,15 @@
+// Description:
+// Write a function which calculates the average of the numbers in a given array.
+
+// Note: Empty arrays should return 0.
+
+
+// My solution
+function findAverage(array) {
+    if (array.length === 0) return 0
+    let sum = 0
+    for ( let i = 0; i < array.length; i++ ) {
+      sum += array[i]
+    }
+    return sum / array.length
+  }
