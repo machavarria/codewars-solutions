2f155dcc2965530a16e189c7e39b98f70baa56e2
Marc Chavarria <marc.a.chavarria3@gmail.com>
2025-10-10 09:52:06 -0400

    Added new codewars challenge

---
+++ 8kyu/multiply.js
@@ -0,0 +1,19 @@
+// Description:
+// This code does not execute properly. Try to figure out why.
+
+
+function multiply(a, b){
+     a * b
+}
+
+
+// It takes in two parameters a and b
+// The a * b needs to multiply
+// Leaving it as is will give you an undefined
+// In order to get a result back I would need to put a return
+
+
+// My solution
+function multiply(a, b){
+    return a * b
+}
