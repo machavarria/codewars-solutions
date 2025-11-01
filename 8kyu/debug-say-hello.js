2e5d9fa831b2d78f62573c06fbd1a9b4b5ad8cd2
Marc Chavarria <marc.a.chavarria3@gmail.com>
2025-11-01 16:57:26 -0400

    Added new codewars challenge

---
+++ 8kyu/debug-say-hello.js
@@ -0,0 +1,18 @@
+// Description:
+// Debugging sayHello function
+// The starship Enterprise has run into some problem when creating a program to greet everyone as they come aboard. It is your job to fix the code and get the program working again!
+
+// Example output:
+
+// Hello, Mr. Spock
+
+
+// The function is supposed to say Hello to something
+// Looking at the function to do that I would use a template literal and use backticks for it 
+
+
+
+// My solution
+function sayHello(name){
+    return `Hello, ${name}`
+}
