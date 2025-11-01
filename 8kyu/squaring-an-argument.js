586be588dcca392ae5cfeb2495eb6456941b15bd
Marc Chavarria <marc.a.chavarria3@gmail.com>
2025-11-01 17:09:20 -0400

    Added new codewars challenge

---
+++ 8kyu/squaring-an-argument.js
@@ -0,0 +1,12 @@
+// Description:
+// Now you have to write a function that takes an argument and returns the square of it.
+
+
+// I need to make a function that takes in a number and returns the square of it 
+
+
+
+// My solution
+function square(n){
+    return n * n;
+}
