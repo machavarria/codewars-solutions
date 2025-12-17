ac06d80d4b65eee165a5f57602b233d491a24d41
Marc Chavarria <marc.a.chavarria3@gmail.com>
2025-12-16 20:52:27 -0500

    Added new codewars challenge

---
+++ 7kyu/anagram-detection.js
@@ -0,0 +1,20 @@
+// Description:
+// An anagram is the result of rearranging the letters of a word to produce a new word (see wikipedia).
+
+// Note: anagrams are case insensitive
+
+// Complete the function to return true if the two arguments given are anagrams of each other; return false otherwise.
+
+// Examples
+// "foefet" is an anagram of "toffee"
+
+// "Buckethead" is an anagram of "DeathCubeK"
+
+
+// My Solution
+var isAnagram = function(test, original) {
+  const normalize = str  =>
+  str.toLowerCase().split("").sort().join("");
+  
+  return normalize(test) === normalize(original)
+};
